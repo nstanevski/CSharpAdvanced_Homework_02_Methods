06942752a1a565a40858d69f8137d98a5dc2f0fb
nstanevski <nikolay.stanevski@gmail.com>
2015-05-08 09:50:12 +0300

    task#2 added

---
+++ 02_LastDigitOfNumber/LastDigitOfNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class LastDigitOfNumber
+{
+    private static string GetLastDigitAsWord(int num)
+    {
+        string[] digitWords = { "zero", "one", "two", "three", "four",
+                                  "five","six","seven","eight","nine"};
+
+        int remainder = num % 10;
+
+        return digitWords[remainder];
+    }
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetLastDigitAsWord(number));
+    }
+}
