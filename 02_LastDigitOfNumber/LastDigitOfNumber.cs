1a31c4205864a323f12841d419f0535ec4224218
nstanevski <nikolay.stanevski@gmail.com>
2015-05-08 09:52:05 +0300

    task#2 - solution improved

--- 02_LastDigitOfNumber/LastDigitOfNumber.cs
+++ 02_LastDigitOfNumber/LastDigitOfNumber.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+/*
+ * Write a method that returns the last digit of a given integer as an English word. 
+ * Test the method with different input values. Ensure you name the method properly.
+ */
 class LastDigitOfNumber
 {
     private static string GetLastDigitAsWord(int num)
